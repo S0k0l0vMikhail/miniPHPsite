110d29097a189b9e6b9675bc81adcce618c6d723
S0k0l0vMikhail <dolgopeh@gmail.com>
2017-06-19 16:11:47 +0300

    мелочи
    чуть чуть наполнил контентом и стили им

--- index.php
+++ index.php
@@ -24,7 +24,29 @@
      </header>
 
      <div id="wrapper">
-       <div id="leftCol">f
+       <div id="leftCol">
+         <div id="bigArticle">
+           <img src="/img/article_1.jpg" alt="Статья 1" title="Статья 1">
+           <h2>Статья 1</h2>
+             <p>Текст 1</p>
+             <a href="/article.php"></a>
+              <div class="more">Далее</div>
+         </div>
+         <div class="clear"></br></div>
+         <div class="article">
+           <img src="/img/article_2.jpg" alt="Статья 2" title="Статья 2">
+           <h2>Статья 2</h2>
+             <p>Текст 2</p>
+             <a href="/article.php"></a>
+              <div class="more">Далее</div>
+         </div>
+         <div class="article">
+           <img src="/img/article_3.jpg" alt="Статья 3" title="Статья 3">
+           <h2>Статья 3</h2>
+             <p>Текст 3</p>
+             <a href="/article.php"></a>
+              <div class="more">Далее</div>
+         </div>
        </div>
 
        <div id="rightCol">
