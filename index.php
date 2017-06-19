86f52e3e65e63e4589d782a29ab8d4fd14bb82f6
S0k0l0vMikhail <dolgopeh@gmail.com>
2017-06-19 14:13:12 +0300

    начало
    шапка + стили

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="windows-1251">
+  <title>Новости</title>
+  <link rel="stylesheet" type="text/css" href="/css/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+     <header>
+       <div id="logo">
+         <a href="/" title="Перейти на главную"><span>Н</span>овости</a>
+       </div>
+       <div id="menuHead">
+          <a href="/about.php">
+            <div style="margin-right: 5%">О нас</div></a>
+          <a href="/feedback.php">
+            <div>Обратная связь</div></a>
+       </div>
+       <div id="regAuth">
+          <A href="/reg.php">Регистрация<a/> <a href="/auth.php">Авторизаця</a>
+       </div>
+       </head>
+     </header>
+</body>
+</html>
